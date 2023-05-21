ebde5a24cf122fd3e1e5c456cf57ae0b582e2006
SLASHLogin <loginmlgxd@gmail.com>
2023-05-21 14:00:54 +0200

    Init new project with base graphql client

---
+++ CountriesList/Program.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using CountriesList;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services
+.AddContinentsClient()
+.ConfigureHttpClient(client => client.BaseAddress = new Uri("https://countries.trevorblades.com/"));
+
+await builder.Build().RunAsync();
